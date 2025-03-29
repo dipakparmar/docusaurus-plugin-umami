52359157da69eea5915ad520447826670e6e944d
Dipak Parmar <hi@dipak.tech>
2025-03-28 18:50:50 -0700

    refactor(options): make PluginOptions properties optional

--- src/options.ts
+++ src/options.ts
@@ -1,13 +1,13 @@
 export type PluginOptions = {
   websiteID: string;
   analyticsDomain: string;
-  scriptName: string;
-  dataHostURL: string;
-  dataAutoTrack: boolean;
-  dataDoNotTrack: boolean;
-  dataCache: boolean;
-  dataDomains: string;
-  dataExcludeSearch: boolean;
+  scriptName?: string;
+  dataHostURL?: string;
+  dataAutoTrack?: boolean;
+  dataDoNotTrack?: boolean;
+  dataCache?: boolean;
+  dataDomains?: string;
+  dataExcludeSearch?: boolean;
 };
 
 export type Options = Partial<PluginOptions>;
